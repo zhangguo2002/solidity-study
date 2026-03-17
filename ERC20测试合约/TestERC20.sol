d13b6c969a197bac34b78498d764571d849b1eba
zhangguo2002 <1792203237@qq.com>
2026-03-18 00:16:20 +0800

    feat:跨链桥、闪电贷、去中心化交易所合约

---
+++ ERC20测试合约/TestERC20.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract TestERC20 is ERC20 {
+    constructor(string memory, string memory symbol) ERC20(name,symbol) {
+        _mint(_msgSender(), 100 ether);
+    }
+}
